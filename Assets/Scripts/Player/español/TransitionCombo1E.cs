0f1bc2c1730e026a5b3bb2b93900cb661c67fd27
NikolasH03 <hurtadovargasnicolas@gmail.com>
2024-09-28 18:06:51 -0500

    funciona el cambio de protagonista
    ya funciona correctamente el cambio de protagonista, por ahora no se han encontrado errores nuevos

---
+++ Assets/Scripts/Player/español/TransitionCombo1E.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionCombo1E : StateMachineBehaviour
+{
+    private Player player;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        player = animator.GetComponent<Player>();
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player.numero_golpesDebiles = 0;
+        player.numero_golpesFuertes = 0;
+    }
+
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player.atacando = false;
+        player.atacandoFuerte = false;
+        player.atacandoDebil = false;
+    }
+
+
+}
